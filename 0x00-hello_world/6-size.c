45942d8b8b0edd0d7561691ac6077ceebf6525f3
kyle-d-badest <kasikay360@gmail.com>
2023-02-16 22:04:54 -0800

    KYLE THE C PROGRAMMER

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
+
 	printf("Size of char: %1u byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
 	printf("SIze of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
