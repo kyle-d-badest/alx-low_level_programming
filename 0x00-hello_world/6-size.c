1ee4ab345d91370c36886db0f38c1a43936f837a
kyle-d-badest <kasikay360@gmail.com>
2023-02-16 21:33:34 -0800

    KYLE C BEGINNER

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printf("Size of char: %1u byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
+	printf("SIze of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
